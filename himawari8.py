034968d336f1f62756ff099c3470a3f1300ef6c9
Yuan Alvin Fu <casouri@gmail.com>
2017-07-28 03:25:58 +0800

    Add time range, fix bugs

--- himawari8.py
+++ himawari8.py
@@ -3,13 +3,13 @@
 from os import environ, system
 import json
 from io import BytesIO
-from time import sleep
+from datetime import time, datetime
 
 import requests
 from PIL import Image
 
-IMG_DIR = environ['HOME'] + '/himawari8_img.png'
-
+IMG_DIR = environ['HOME']
+DAY_IMG_PATH = '/Users/yuan/.himawari8/cowboy.jpg'
 
 def gen_img():
     config_url = 'http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json'
@@ -18,7 +18,6 @@ def gen_img():
 
     curr_date = json.loads(requests.get(config_url).text)['date']
     # example: {"date":"2017-07-22 04:50:00","file":"PI_H08_20170722_0450_TRC_FLDK_R10_PGPFD.png"}
-    tmp_path = '/tmp/earth.png'
     format_date = curr_date.replace('-', '/').replace(' ', '/').replace(
         ':', '')
     # from 2017-07-22 04:50:00 to 2017/07/22/045000
@@ -43,17 +42,33 @@ def gen_img():
     img.paste(img_lis[2], (517, 600))
     img.paste(img_lis[3], (1067, 600))
 
-    img.save(IMG_DIR)
+    img_path = IMG_DIR + '/.' + curr_date.replace(' ', '-').replace(':', '-') + '.png'
+    img.save(img_path)
+    return img_path
 
-
-def set_desktop():
+def set_desktop(img_path):
     system(
         "osascript -e 'tell application \"Finder\" to set desktop picture to \"{}\" as POSIX file'".
-        format(IMG_DIR))
+        format(img_path))
+def in_range(t1, t2):
+    '''
+    t1, t2 are time range in where script doen not run
+    in that range the desktop will be set to another image
+    t1, t2 are tuple
+    24 hour mode
+    10:24 --> (10, 24)
+    '''
+    now = datetime.now()
+    now_time = now.time()
+    if now_time >= time(t1[0], t1[1]) and now_time <= time(t2[0], t2[1]):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    while True:
-        sleep(600)
-        gen_img()
-        set_desktop()
+    if in_range((7,0), (17,0)):
+        set_desktop(DAY_IMG_PATH)
+    else:
+        img_path = gen_img()
+        set_desktop(img_path)
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp himawari8.py ~/.himawari8.py
+cp himawari8.py ~/.himawari8/himawari8.py
+cp cowboy.jpg ~/.himawari8/cowboy.jpg
 
 first='<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -19,11 +20,12 @@ first='<?xml version="1.0" encoding="UTF-8"?>
     </dict>
 </plist>
 '
-second=$HOME/.himawari8.py
+second=$HOME/.himawari8/himawari8.py
 plist=${first/script_p/$second}
 
 rm $HOME/Library/LaunchAgents/com.yuan.himawari8.plist
 echo $plist >> $HOME/Library/LaunchAgents/com.yuan.himawari8.plist
 
+launchctl unload $HOME/Library/LaunchAgents/com.yuan.himawari8.plist
 launchctl load $HOME/Library/LaunchAgents/com.yuan.himawari8.plist
-chmod 744 ~/.himawari8.py
+chmod 744 ~/.himawari8/himawari8.py
